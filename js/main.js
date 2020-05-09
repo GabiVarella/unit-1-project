d3a644e0b7e8337628728ea013da6945ce33a577
Gabriel Varella <gabrielvarella@Gabriels-MacBook-Pro.local>
2020-05-09 15:32:32 -0700

    basic html and js pseudo code

---
+++ js/main.js
@@ -0,0 +1,43 @@
+/*==>==>==>==> CONSTANTS ==>==>==>==>*/
+
+const easyWords = [apple, baloon, circle, beard, sweet];
+const mediumWords = [garbage, computer, mattress,];
+const hardWords = [photograph, neighborhood, dashboard, pharmacist];
+
+
+/*==>==>==>==> APP STATE (VARIABLES) ==>==>==>==>*/
+let secretWord; 
+let score; 
+let timeLeft;  
+
+
+/*==>==>==>==> CACHED ELEMENTS REFERENCES ==>==>==>==>*/
+
+
+
+/*==>==>==>==> EVENT LISTENERS ==>==>==>==>*/
+
+
+
+/*==>==>==>==> FUNCTIONS ==>==>==>==>*/
+Init();
+
+function selectWord(){
+    //check which difficulty btn was clicked
+    //select ranmdomly from respective array
+    //
+};
+
+function checkWinner(){
+    //returns win/lose logic
+};
+
+function render(){
+    //display selecWord() choice to secret-word HTML elem
+    //update the secret-word HTML elem
+    //update wrong-guesses HTML elem
+    //update chances-left HTML elem
+    //update score
+    //update time left
+
+};
