95a73060eb4574dafa0de179a9e2b2b15b46fea7
Gabriel Varella <gabrielvarella@Gabriels-MacBook-Pro.local>
2020-05-10 14:42:00 -0700

    add more cached elems and more detailed pseudocode

--- js/main.js
+++ js/main.js
@@ -4,22 +4,24 @@ const easyWords = ["apple", "baloon", "circle", "beard", "sweet"];
 const medWords = ["garbage", "computer", "mattress"];
 const hardWords = ["javascript", "photograph", "neighborhood", "dashboard", "pharmacist"];
 
+//Fill in with image links later
 const imageLookup = {
-    "default" :
-    "first" :
-    "second" :
-    "third" :
-    "fourth" :
-    "fifth" :
+    "default" : "",
+    "first" : "",
+    "second" : "",
+    "third" : "",
+    "fourth" : "",
+    "fifth" : "",
 };
 
 
 /*==>==>==>==> APP STATE (VARIABLES) ==>==>==>==>*/
-let secretWord; 
+let secretWord; //needs to be a string??
 let score; 
 let timeLeft;  
 let chancesLeft;
 let correctGuesses;
+let answerArr;
 //maybe let remainingLetters;
 
 
@@ -32,6 +34,8 @@ const rstBtnEl = document.getElementById("resetbtn");
 const letterBtnsElm = document.getElementById("letters-div");
 const secretWordEl = document.getElementById("secret-word");
 const imageEl = document.getElementById("image");
+const wrongChoicesEl = document.getElementById("wrong-choices");
+const choicesLeftEl = document.getElementById("chances-left");
 //Maybe not byclassname but queryselector
 
 
@@ -66,18 +70,30 @@ function init(){
     //set score to 0
     score = 0;
     //diplay image to none
-    imageEl.innerText = imageLookup[]
+    imageEl.innerText = imageLookup["default"];
     //set secret word to ""
+    secretWord = "";
 };
 function selectWord(){
     //check which difficulty btn was clicked
-    //select ranmdomly from respective array
-    //displays on secret word html div
+    //select randomly from chosen dificulty array
+    //secretWord = difArrayclickeds[Math.floor(Math.random() * difArrayclickeds.length)];
+    //let answerArr = secretWord.split(''); into an array of letters
+    //turn answerArr into "_" by answer.forEach((element) => {answer.element = "_";
+    //});
+    
 };
 
 function checkLetter(){
-    //1-check if evt.target.innerText is included in secret word
-    //2- if true display letter
+    //1- get evt.target.innerText, uppercase it(also put it in a variable????)
+    //2- CONDITIONAL -- check to see if evt.target.innerText is included in secret word
+    //3- if true:
+    // * display letter
+    //4- If false:
+    // * subtract 1 from chancesLeft
+    // * Display chances left on chances-left HTML div
+    // * Dispay in HTML wrong-choice div
+    // * Access and display correspondent img from imageLookup obj
     //render();
 };
 
@@ -94,3 +110,4 @@ function render(){
     //update time left
 
 };
+
